42b75f291938c0495d505ab1ecdec8b4286093f5
AndreyTW <113095182+AndreyTW@users.noreply.github.com>
2023-05-16 18:05:17 +0300

    Application was divided into ApplicationConsole and ApplicationTelegram (#2)

---
+++ roman-calculator-core/src/test/kotlin/ru/andreyTw/romanCalculator/core/InputExpressionProcessorShould.kt
@@ -0,0 +1,22 @@
+package ru.andreyTw.romanCalculator.core
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import ru.andreyTw.romanCalculator.core.InputExpressionProcessor.Companion.processExpression
+
+class InputExpressionProcessorShould {
+    @Test
+    fun warnAboutEnteringAnInvalidExpression() {
+        assertEquals("\nError! Wrong equation is given!", processExpression("I+-V"))
+    }
+
+    @Test
+    fun warnAboutEnteringAnInvalidRomanNumber() {
+        assertEquals("\nError! Incorrect roman number is given!", processExpression("IIV+C"))
+    }
+
+    @Test
+    fun `formResultString_CLXXIII_whenExpression_IV+CLXIX_isGiven`() {
+        assertEquals("CLXXIII", processExpression("IV+CLXIX"))
+    }
+}
