7853139144a1a7746ff1321007044c3eac49e70d
AndreyTW <sav_687@mail.ru>
2023-05-17 09:18:57 +0300

    main and tg threads were introduced

--- roman-calculator-telegram/src/main/kotlin/ru/andreyTw/romanCalculator/telegram/ApplicationTelegram.kt
+++ roman-calculator-telegram/src/main/kotlin/ru/andreyTw/romanCalculator/telegram/ApplicationTelegram.kt
@@ -4,31 +4,41 @@ import com.pengrad.telegrambot.TelegramBot
 import com.pengrad.telegrambot.UpdatesListener
 import com.pengrad.telegrambot.request.SendMessage
 import ru.andreyTw.romanCalculator.core.InputExpressionProcessor.Companion.processExpression
+import sun.misc.Signal
 import java.io.File
+import kotlin.system.exitProcess
+
+val bot = TelegramBot(File("botKey").readText())
 
 fun main() {
+    println("Application is starting!")
+    init()
+    Signal.handle(Signal("INT")) {
+        shutdown()
+        println("Application is closing!")
+        exitProcess(0)
+    }
+    while (true) {
+    }
+}
 
-    val bot = TelegramBot(File("botKey").readText())
-    var exitFlag = false
+private fun init() {
+    println("Telegram bot is initializing...")
+
     bot.setUpdatesListener { updates ->
         updates.forEach {
             val message = it.message().text()
             println("Expression \"$message\" was successfully received")
-            if (message == "exit") {
-                exitFlag = true
-                bot.removeGetUpdatesListener()
-                bot.shutdown()
-            } else {
-                exitFlag = false
-                val result = processExpression(message)
-                bot.execute(SendMessage(it.message().chat().id(), result))
-            }
+            val result = processExpression(message)
+            bot.execute(SendMessage(it.message().chat().id(), result))
         }
         UpdatesListener.CONFIRMED_UPDATES_ALL
     }
+}
 
-    while (!exitFlag) {
-        println(exitFlag)
-    }
+private fun shutdown() {
+    println("Telegram bot is shutting down...")
 
+    bot.removeGetUpdatesListener()
+    bot.shutdown()
 }
