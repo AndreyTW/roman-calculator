0875fc287a5ec9b5634b977e1b298bfea5aa08a6
Andrey_TW <sav_687@mail.ru>
2023-06-04 19:22:39 +0300

    TelegramBotWrapper test implementation preparation

--- roman-calculator-telegram/src/test/kotlin/ru/andreyTw/romanCalculator/telegram/TelegramBotWrapperShould.kt
+++ roman-calculator-telegram/src/test/kotlin/ru/andreyTw/romanCalculator/telegram/TelegramBotWrapperShould.kt
@@ -2,18 +2,40 @@ package ru.andreyTw.romanCalculator.telegram
 
 import org.junit.jupiter.api.Test
 
+//@ExtendWith(MockitoExtension::class)
 class TelegramBotWrapperShould {
+
+//    private lateinit var telegramBotWrapper: TelegramBotWrapper
+//
+//    @Mock
+//    private lateinit var key: String
+//
+//    @Mock
+//    private lateinit var ownerId: String
+//
+//    @Mock
+//    private lateinit var processor: (message: String) -> String
+//
+//    @BeforeEach
+//    fun setUp() {
+//        telegramBotWrapper = getTelegramBotWrapper(key, ownerId) { processor(it) }
+//    }
 
     @Test
     fun initializeBot() {
+//        telegramBotWrapper.init()
 
     }
 
     @Test
     fun sendMessageFromBot() {
+//        telegramBotWrapper.sendMessage()
+
     }
 
     @Test
     fun shutdownBot() {
+//        telegramBotWrapper.shutdown()
+
     }
 }
