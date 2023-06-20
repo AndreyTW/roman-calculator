f014617d515e8524f82b5d4734763407554b0747
AndreyTW <sav_687@mail.ru>
2023-06-20 19:41:32 +0300

    reflection was used in TelegramBotWrapperShould

--- roman-calculator-telegram/src/test/kotlin/ru/andreyTw/romanCalculator/telegram/TelegramBotWrapperShould.kt
+++ roman-calculator-telegram/src/test/kotlin/ru/andreyTw/romanCalculator/telegram/TelegramBotWrapperShould.kt
@@ -1,6 +1,7 @@
 package ru.andreyTw.romanCalculator.telegram
 
 import com.pengrad.telegrambot.TelegramBot
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
@@ -8,6 +9,8 @@ import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.any
 import org.mockito.kotlin.verify
 import ru.andreyTw.romanCalculator.telegram.TelegramBotWrapper.Companion.getTelegramBotWrapper
+import java.lang.reflect.Field
+import java.lang.reflect.Modifier
 
 @ExtendWith(MockitoExtension::class)
 class TelegramBotWrapperShould {
@@ -17,18 +20,27 @@ class TelegramBotWrapperShould {
     @Mock
     private lateinit var telegramBot: TelegramBot
 
-    //@BeforeEach
+    @BeforeEach
     fun setUp() {
         //
         telegramBotWrapper = getTelegramBotWrapper("", "") { "" }
         val botField = telegramBot.javaClass.getDeclaredField("api")
         botField.isAccessible = true
+        val modifiers = Field::class.java.getDeclaredField("modifiers")
+        modifiers.isAccessible = true
+        modifiers.setInt(botField, botField.modifiers.and(Modifier.FINAL.inv()))
+
+
+
+
+
+
         botField.set(telegramBotWrapper, telegramBot)
         botField.isAccessible = false
     }
 
 
-    //@Test
+    @Test
     fun initializeBot() {
         telegramBotWrapper.init()
         verify(telegramBot.setUpdatesListener(any()))
